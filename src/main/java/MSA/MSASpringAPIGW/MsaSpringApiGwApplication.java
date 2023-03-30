349dc2f01e467fa19e438c3db990e5f6d19f148c
seungkim <seungkim@hist.co.kr>
2023-03-30 17:07:33 +0900

    MSA-Spring-API-GateWay-Code

---
+++ src/main/java/MSA/MSASpringAPIGW/MsaSpringApiGwApplication.java
@@ -0,0 +1,13 @@
+package MSA.MSASpringAPIGW;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsaSpringApiGwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsaSpringApiGwApplication.class, args);
+	}
+
+}
---
+++ src/test/java/MSA/MSASpringAPIGW/MsaSpringApiGwApplicationTests.java
@@ -0,0 +1,13 @@
+package MSA.MSASpringAPIGW;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsaSpringApiGwApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
